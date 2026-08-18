986ad634a174fe21ebf329fe960831d8ba63c5f0
Eberson dos Santos Cosme <eberson.cosme@apoioprodesp.sp.gov.br>
2026-08-18 15:46:00 -0300

    Formhelper

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -4,55 +4,51 @@
         <h2 class="text-2xl font-bold mb-6 p-3">Create user</h2>
 
         <form class="space-y-4 p-3" @submit.prevent="create">
+            <span v-if="form.isDirty">Unsaved changes</span>
             <div class="form-group mb-3">
                 <label class="form-label block text-sm font-medium mb-1">Name</label>
-                <input type="text" class="form-control w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your name" v-model="user.name" />
-                <span v-if="errors.name" class="text-danger" v-text="errors.name"></span>
+                <input type="text" class="form-control w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your name" v-model="form.name" />
+                <span v-if="form.errors.name" class="text-danger" v-text="form.errors.name"></span>
             </div>
 
             <div class="form-group mb-3">
                 <label class="form-label block text-sm font-medium mb-1">Email</label>
-                <input type="text" class="form-control w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your email" v-model="user.email"/>
-                <span v-if="errors.email" class="text-danger" v-text="errors.email"></span>
+                <input type="text" class="form-control w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your email" v-model="form.email"/>
+                <span v-if="form.errors.email" class="text-danger" v-text="form.errors.email"></span>
             </div>
 
             <div class="form-group mb-3">
                 <label class="form-label block text-sm font-medium mb-1">Password</label>
-                <input type="password" class="form-control w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your password" v-model="user.password" />
-                <span v-if="errors.password" class="text-danger" v-text="errors.password"></span>
+                <input type="password" class="form-control w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Enter your password" v-model="form.password" />
+                <span v-if="form.errors.password" class="text-danger" v-text="form.errors.password"></span>
             </div>
 
-            <button type="submit" class="btn btn-primary w-full py-2 px-4 rounded hover:bg-blue-700 transition" :disabled="creating">
+            <button type="submit" class="btn btn-primary w-full py-2 px-4 rounded hover:bg-blue-700 transition" :disabled="form.processing || !form.isDirty">
                 Register
             </button>
         </form>
     </div>
+    <Link href="/users/create" class="btn btn-primary btn-lg">Refresh</Link>
 </template>
 
 <script setup>
-import { reactive, ref } from 'vue';
-import { router, usePage } from '@inertiajs/vue3';
+import { Link, useForm } from '@inertiajs/vue3';
 
-const user = reactive({
+const form = useForm({
     name: '',
     email: '',
     password: ''
-})
-
-let creating = ref(false);
-
-defineProps({
-    errors: Object
 });
 
 function create() {
-    router.post('/users', user, {
-        onStart(){
-            creating.value = true;
+    form.post('/users', {
+        onFinish: () => {
+            setTimeout(() => {
+                form.clearErrors();
+            }, 3000);
+            //form.reset(); //basta passar os campos como parametro
         },
-        onFinish(){
-            creating.value = false;
-        }
+        preserveScroll: true // Preserva o scroll da tela
     });
 }
 </script>
